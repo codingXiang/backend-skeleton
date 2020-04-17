28abc2bb5fb7d9b7e893508109a22968d1ef910a
阿翔 <codingxiang@smail.nchu.edu.tw>
2020-04-17 18:04:02 +0800

    移除檔案

--- module/startup/repository.go
+++
@@ -1,8 +0,0 @@
-package startup
-
-//Repository 用於與資料庫進行存取的封裝方法
-type Repository interface {
-	/*
-	加入方法
- 	*/
-}
--- module/startup/service.go
+++
@@ -1,8 +0,0 @@
-package startup
-
-//Service 用於封裝商業邏輯的方法
-type Service interface {
-	/*
-	加入方法
- 	*/
-}
